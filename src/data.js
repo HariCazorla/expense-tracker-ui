baef93f1ef052c2d00fd9cc7c1329b8ddef16c82
vagrant <vagrant@ubuntu2004.localdomain>
2021-08-19 21:16:50 +0000

    Implement nav bar

---
+++ src/data.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const links = [
+  {
+    id: 1,
+    url: "/",
+    text: "expenses",
+  },
+  {
+    id: 2,
+    url: "/categories",
+    text: "categories",
+  },
+];
