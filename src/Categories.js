c1665bf46c8bf9b4164522a4e73af39518859f9e
vagrant <vagrant@ubuntu2004.localdomain>
2021-08-20 21:37:03 +0000

    call getcategories api

--- src/Categories.js
+++ src/Categories.js
@@ -1,6 +1,19 @@
 import React from "react";
 
 function Categories() {
+  const [categories, setCategories] = React.useState([]);
+  React.useEffect(() => {
+    var headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    headers.append("Access-Control-Allow-Origin", "*");
+    headers.append("Authorization", "Basic " + window.btoa("admin:admin"));
+    const url = "http://localhost:8080/api/v1/category";
+    fetch(url, { headers: headers })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
   return (
     <div>
       <h1>Categories</h1>
